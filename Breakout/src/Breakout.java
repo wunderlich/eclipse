3eda20d1d65760294af937ecf19677d2d0334616
Daniel Wunderlich <code@wu-web.de>
2017-03-29 10:55:44 +0200

    Fixed changing initSpeed. Added speedUpFactor.

--- Breakout/src/Breakout.java
+++ Breakout/src/Breakout.java
@@ -9,6 +9,7 @@ public class Breakout extends PApplet {
 	Control control = Control.KEYBOARD;
 
 	int noBricks = 3;
+	float speedUpFactor = 1.2f;
 	
 	PVector initSpeed = new PVector(5, -2);
 	
@@ -32,7 +33,7 @@ public class Breakout extends PApplet {
 		size(800, 900);
 	}
 
-	public void setup(){
+	public void setup(){		
 		
 		state = State.SETUP;
 		
@@ -45,6 +46,7 @@ public class Breakout extends PApplet {
 		background(0);
 		
 		text(state.name(), 15, 15);
+		text("speedX: " + ball.getSpeedX() + ", speedY: " + ball.getSpeedY(), 15, 30);
 
 		if (state == State.PLAY) {
 			ball.bounceBorder();
@@ -166,9 +168,9 @@ public class Breakout extends PApplet {
 	void setupGame() {
 		state = State.SETUP;
 		pad = new Paddle(this, width/2, height - 100, 100, 20, color(255), color(0, 255, 255), new PVector(10, 0));
-		ball = new Ball(this, width/2, height - 150, 30, color(0, 255, 0), color(0, 255, 0), initSpeed);
+		ball = new Ball(this, width/2, height - 150, 30, color(0, 255, 0), color(0, 255, 0), initSpeed.copy());
 		
-		ball.setSpeed(initSpeed);
+		//ball.setSpeed(initSpeed);
 		
 		activeBricks = noBricks;
 		
@@ -190,6 +192,7 @@ public class Breakout extends PApplet {
 				if (ball.bounceAt(b)) {
 					b.setActive(false);
 					activeBricks--;
+					ball.setSpeed(ball.getSpeed().mult(speedUpFactor));
 				}
 			}
 		}
